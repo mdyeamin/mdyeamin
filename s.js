b5639f1c9d3245968058020c55cbd91e8734d291
Md Yeamin <ahmedyeamin05@gmail.com>
2022-03-24 02:25:36 +0600

    semy final

---
+++ s.js
@@ -0,0 +1,9 @@
+const sr = function repeatNuN(fn,rn){
+const srity = rn/fn*fn-rn;
+return srity
+}
+console.log(sr);
+function repeatLove(lx,xx){
+    return undefined;
+}
+const XR = repeatNuN(repeatLove)
